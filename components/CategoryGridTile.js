219126a5a2449dfaaf8d4353a04b31af0676e26a
ihlasMert <ihlas1mert@gmail.com>
2023-05-24 18:01:00 +0300

    created meal project

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,51 @@
+import { Pressable, View, Text, StyleSheet } from "react-native";
+
+function CategoryGridTile({ title, color }) {
+  return (
+    <View style={styles.gridItem}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
+          <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    backgroundColor: "white",
+  },
+  button: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight:"bold"
+  },
+  buttonPressed: {
+    opacity: 0.9,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
